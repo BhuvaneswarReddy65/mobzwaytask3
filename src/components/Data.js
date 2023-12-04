0090d95b3bfe030836bbab7a6033055736f6a4c9
Bhuvaneswar <bhuvaneswar5251@gmail.com>
2023-12-04 12:45:20 +0530

    all clear

---
+++ src/components/Data.js
@@ -0,0 +1,48 @@
+// {/* <div className="table-container">
+//   <table>
+//     <thead>
+//       <tr>
+//         <th>
+//           <span>Id</span>
+//         </th>
+//         <th>
+//           <span>Customer</span>
+//         </th>
+//         <th>
+//           <span>Lastseen</span>
+//         </th>
+//         <th>
+//           <span>Orders</span>
+//         </th>
+//         <th>
+//           <span>Total_spent</span>
+//         </th>
+//         <th>
+//           <span>Latest_purchase</span>
+//         </th>
+//         <th>
+//           <span>News</span>
+//         </th>
+//         <th>
+//           <span>Segment</span>
+//         </th>
+//       </tr>
+//     </thead>
+//     <tbody>
+//       {Data.map((cus) => {
+//         return (
+//           <tr>
+//             <td>{cus.id}</td>
+//             <td>{cus.customer}</td>
+//             <td>{cus.lastseen}</td>
+//             <td>{cus.orders}</td>
+//             <td>{cus.total_spent}</td>
+//             <td>{cus.latest_purchase}</td>
+//             <td>{cus.news}</td>
+//             <td>{cus.segment}</td>
+//           </tr>
+//         );
+//       })}
+//     </tbody>
+//   </table>
+// </div>; */}
